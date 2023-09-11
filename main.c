a60a18b2a6e8a9903fa944c4654725e96340f46b
Ceyhun <69029757+Ceyhun-Tufan@users.noreply.github.com>
2023-09-11 17:05:32 +0300

    CheckingFolders
    Var olan/olmayan folder kontrolü. Hatasız çalışıyor.

--- main.c
+++ main.c
@@ -4,8 +4,10 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <signal.h>
 #include <locale.h>
+
 
 void moveFile(const char *sourcePath, const char *destinationPath) {
     if (rename(sourcePath, destinationPath) != 0) {
@@ -14,6 +16,33 @@ void moveFile(const char *sourcePath, const char *destinationPath) {
         printf("\033[0;33m%s named file transferred to :  %s \033[0m \n", sourcePath, destinationPath);
     }
 }
+
+
+void createFolders(const char *sourcePath){
+   
+   const char arr[4][21] = {"MetinBelgeleri","Exeler","Resimler","RarDosyalari"};
+   char checkerPath[256];
+   int check;
+
+   for(int i = 0;i<4;i++){
+      snprintf(checkerPath, sizeof(checkerPath), "%s/%s", sourcePath,arr[i]);
+      DIR *checker = opendir(checkerPath);
+      if(checker){
+         printf("%s acilip kapandi\n",arr[i]);
+         closedir(checker);
+      }else{
+         check = mkdir(checkerPath);
+         if(!check){
+            printf("%s created.\n",arr[i]);
+         }else{
+            printf("Error");
+         }
+      }
+   }
+
+   
+}
+
 
 void FileOrganizer(const char *sourceDir){
    DIR *dir;
@@ -30,30 +59,32 @@ void FileOrganizer(const char *sourceDir){
             // bu fonskiyon sourcePath'e sığabileceği kadar miktarda güvenli
             // şekilde istenilen karakterleri yükler
             if (strstr(entry->d_name, ".txt") != NULL) { // dosya uzantısını kontrol eder
-                     snprintf(destinationPath, sizeof(destinationPath), "C:/Users/ASUS/Downloads/MetinBelgeleri/%s", entry->d_name);
+                     snprintf(destinationPath, sizeof(destinationPath), "%s/MetinBelgeleri/%s", sourceDir,entry->d_name);
+                     printf("%s",destinationPath);
                      moveFile(sourcePath, destinationPath);
             }else if(strstr(entry->d_name, ".exe") != NULL){
-                     snprintf(destinationPath, sizeof(destinationPath), "C:/Users/ASUS/Downloads/Exeler/%s", entry->d_name);
+                     snprintf(destinationPath, sizeof(destinationPath), "%s/Exeler/%s", sourceDir,entry->d_name);
                      moveFile(sourcePath, destinationPath);
             }else if(strstr(entry->d_name, ".jpg") != NULL ){
-                     snprintf(destinationPath, sizeof(destinationPath), "C:/Users/ASUS/Downloads/Resimler/%s", entry->d_name);
+                     snprintf(destinationPath, sizeof(destinationPath), "%s/Resimler/%s",sourceDir, entry->d_name);
                      moveFile(sourcePath, destinationPath);
             }else if(strstr(entry->d_name, ".png") != NULL ){
-                     snprintf(destinationPath, sizeof(destinationPath), "C:/Users/ASUS/Downloads/Resimler/%s", entry->d_name);
+                     snprintf(destinationPath, sizeof(destinationPath), "%s/Resimler/%s",sourceDir, entry->d_name);
                      moveFile(sourcePath, destinationPath);
             }else if(strstr(entry->d_name, ".jpeg") != NULL ){
-                     snprintf(destinationPath, sizeof(destinationPath), "C:/Users/ASUS/Downloads/Resimler/%s", entry->d_name);
+                     snprintf(destinationPath, sizeof(destinationPath), "%s/Resimler/%s",sourceDir, entry->d_name);
                      moveFile(sourcePath, destinationPath);
             }else if(strstr(entry->d_name, ".zip") != NULL ){
-                     snprintf(destinationPath, sizeof(destinationPath), "C:/Users/ASUS/Downloads/RarDosyalari/%s", entry->d_name);
+                     snprintf(destinationPath, sizeof(destinationPath), "%s/RarDosyalari/%s",sourceDir, entry->d_name);
                      moveFile(sourcePath, destinationPath);
             }else if(strstr(entry->d_name, ".rar") != NULL ){
-                     snprintf(destinationPath, sizeof(destinationPath), "C:/Users/ASUS/Downloads/RarDosyalari/%s", entry->d_name);
+                     snprintf(destinationPath, sizeof(destinationPath), "%s/RarDosyalari/%s",sourceDir, entry->d_name);
                      moveFile(sourcePath, destinationPath);
             }
          }
       }
       closedir(dir);
+      printf("File closed.");
    }else{
       printf("\033[0;31mThere is an error while opening the directory!\033[0m \n");
    }
@@ -61,9 +92,18 @@ void FileOrganizer(const char *sourceDir){
 }
 
 int main() {
+   struct FolderNames
+      {
+         char metin[21];
+         char exe[13];
+         char resim[15];
+         char rar[19];
+      }foldernames;
+
    const char *sourceDir = "C:/Users/ASUS/Downloads";
    //signal(SIGINT,cleaner);
    printf("\033[0;36mOrganizer started!\033[0m \n");
+   createFolders(sourceDir);
    FileOrganizer(sourceDir);
    return 0;
 }
